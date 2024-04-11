3d519b0e987412312f815ac0debf24818e9ffdc9
Nery <pedronas@al.insper.edu.br>
2024-04-11 12:03:28 -0300

    ex. 3

--- client.js
+++ client.js
@@ -39,12 +39,23 @@ async function exercicio_2(exercises, config){
       .then((response) => response.data)
 }
 
+// Exercício 3
+async function exercicio_3(exercises, config){
+  ex3 = exercises['nome-do-usuario'];
+  email = ex3['entrada']['email'];
+  arroba = email.search("@")
+  return axios
+      .post("https://tecweb-js.insper-comp.com.br/exercicio/nome-do-usuario", {'resposta': email.slice(0, arroba)}, config)
+      .then((response) => response.data)
+}
+
 async function main(){
     let token = await get_token();
     config["headers"]["Authorization"] = `Bearer ${token}`;
     let exercises = await get_exercises(config);
     await exercicio_1(exercises, config);
     await exercicio_2(exercises, config);
+    await exercicio_3(exercises, config);
     console.log(exercises);
 }
 
